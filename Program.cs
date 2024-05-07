5d3876aa0f73e571c28ededa7bc6e4b549102194
warriorcat34 <finndips279@gmail.com>
2024-05-08 00:32:20 +0300

    domashna

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+
+namespace DomashanClasss
+{
+    internal class Program
+    {
+        //Zad Person
+        static void Main(string[] args)
+        {
+            Console.Write("Write name: ");
+
+            person person = new person();
+            person.Name = Console.ReadLine();
+
+            Console.Write("Write age, bitte: ");
+            person.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"HELLO! my name is {person.Name} and I'm {person.Age} years old!");
+            //predavam tva deto znam i mi e v uma kao za 00:00 chasa, sorry...
+        }
+    }
+}
---
+++ person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DomashanClasss
+{
+    internal class person
+    {
+        string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        int age;
+        public int Age { get { return age; } set { age = value; } }
+
+        
+        
+    }
+}
